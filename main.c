8e4da336f239b58c6aab82e3aea7d2984fee8e35
MahtaFetrat <77fetrat@gmail.com>
2023-01-31 22:52:56 +0330

    fix allocated space not freed

--- main.c
+++ main.c
@@ -7,9 +7,7 @@ int main()
     // Initialize placeholder array for arguments of the input commands.
     char **cmd_args = (char **)malloc(MAX_CMD_ARG_COUNT * sizeof(char *));
     for (int i = 0; i < MAX_CMD_ARG_COUNT; i++)
-    {
         cmd_args[i] = (char *)malloc(MAX_CMD_ARG_LEN * sizeof(char));
-    }
 
     initialize_parser();
 
@@ -21,6 +19,11 @@ int main()
         int cmd_code = parse(input, cmd_args);
         printf("Input command: %d\n", cmd_code);
     } while (1);
+
+    // Free placeholder array for arguments of the input commands.
+    for (int i = 0; i < MAX_CMD_ARG_COUNT; i++)
+        free(cmd_args[i]);
+    free(cmd_args);
 
     return 0;
 }
