8ee6ef865d58fb6f389f09871a8a43f949aa2d07
fatemeh asgari <fatemeh.asgari@tapsi.cab>
2023-02-03 17:39:18 +0330

    Add aliasing commands

--- main.c
+++ main.c
@@ -5,6 +5,12 @@
 #include <unistd.h>
 #include <string.h>
 
+#define MAX_COMMAND_LEN 20
+
+char **predefined_command_keys;
+char **predefined_command_values;
+int command_num;
+
 void print_prompt()
 {
     char prompt[200];
@@ -19,6 +25,44 @@ void print_prompt()
         printf("%s$ ", prompt);
 }
 
+void read_predefined_commands()
+{
+    // printf("start of func\n");
+    FILE *fp;
+    fp = fopen("my_commands.txt", "r");
+
+    predefined_command_keys = (char **)malloc(10 * sizeof(char *));
+    predefined_command_values = (char **)malloc(10 * sizeof(char *));
+    for (int i = 0; i < 10; i++)
+    {
+        predefined_command_keys[i] = (char *)malloc(MAX_COMMAND_LEN * sizeof(char));
+        predefined_command_values[i] = (char *)malloc(MAX_COMMAND_LEN * sizeof(char));
+    }
+
+    // printf("allocated\n");
+
+    int count = 0;
+    char buffer[10][MAX_COMMAND_LEN];
+    char str[MAX_COMMAND_LEN];
+    // printf("got buffer\n");
+    while (EOF != fscanf(fp, "%30[^\n]\n", str))
+    {
+        // printf("str");
+        strcpy(buffer[count], str);
+        count++;
+    }
+    // printf("fulled buffer\n");
+    command_num = count;
+    // printf("count finished\n");
+    for (int i = 0; i < count; i++)
+    {
+        predefined_command_keys[i] = strtok(buffer[i], "::");
+        predefined_command_values[i] = strtok(NULL, "::");
+    }
+    fclose(fp);
+    // printf("func finished\n");
+}
+
 int main()
 {
     int argc;
@@ -27,8 +71,11 @@ int main()
 
     initialize_parser();
     initialize_executor();
+    // printf("parser initialized\n");
+    read_predefined_commands();
 
     char input[MAX_CMD_ARG_COUNT * (MAX_CMD_ARG_LEN + 2)];
+    char predefined_command[MAX_CMD_ARG_COUNT * (MAX_CMD_ARG_LEN + 2)];
     int quitted = 0;
 
     while (!quitted)
@@ -36,7 +83,22 @@ int main()
         print_prompt();
         if (scanf("%[^\n]%*c", input) != 0) // Input line (including white-spaces).
         {
-            int cmd_code = parse(input, &argc, argv);
+            int flag = 0;
+            int cmd_code;
+            // for (int i = 0; i < command_num; i++)
+            // {
+            //     printf("command %d: %s\n", i, predefined_command_keys[i]);
+            //     if (strcmp(input, predefined_command_keys[i]) == 0)
+            //     {
+            //         cmd_code = parse(predefined_command_values[i], &argc, argv);
+            //         flag = 1;
+            //         break;
+            //     }
+            // }
+            if (flag == 0)
+            {
+                cmd_code = parse(input, &argc, argv);
+            }
 
             if (cmd_code == 0) // quit cmd
                 quitted = 1;
